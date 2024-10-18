f4ab5297b82204616ba41fb278f5b1cfe3b5dc39
Sean <65122102+fuy116@users.noreply.github.com>
2024-10-18 15:17:10 +0800

    Initial Commit

---
+++ LiftOpt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiftOpt
+//
+//  Created by Sean Fu on 2024/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiftOpt/LiftOptApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftOptApp.swift
+//  LiftOpt
+//
+//  Created by Sean Fu on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct LiftOptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
